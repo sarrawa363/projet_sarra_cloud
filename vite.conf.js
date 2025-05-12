47e4b9fd490f58a5239a77001be83ec328590658
awasarr <sarrawa363@gmail.com>
2025-05-12 10:31:21 +0000

    fichier  modifie

--- vite.conf.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  root: 'public', // <-- indique à Vite que ton point d'entrée est ici
-  build: {
-    outDir: '../dist', // <-- où mettre les fichiers compilés
-    emptyOutDir: true
-  }
-})
